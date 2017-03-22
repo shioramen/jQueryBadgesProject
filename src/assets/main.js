9f2b45799c193b9d10ae3afa75c46f5f37b722e8
shioramen <esther.t0000@gmail.com>
2017-03-22 15:52:09 -0700

    jquery practice

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,40 @@
 $(function() {
 
-  // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/estherxtan.json',
+    dataType: 'jsonp',
+    success: function(response){
+      addCourses(response.courses.completed);
+    }
+  });
+
+  function addCourses(courses){
+    var $badges = $('#badges');
+    console.log(badges);
+
+    courses.forEach(function(course){
+      console.log(course);
+
+      var $course = $('<div />', {
+        'class': 'course'
+      }).appendTo($badges);
+
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+
+      $('<a />', {
+        'class': 'btn btn-primary',
+        target: '_blank',
+        href: course.url,
+        text: 'See Course'
+      }).appendTo($course);
+    })
+
+  }
 
 });
